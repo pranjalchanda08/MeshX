8fcca6798f7b4a661fd8a5f1dc6d7f8002a59aee
Pranjal <pranjalchanda08@gmail.com>
2024-12-28 20:55:09 +0530

    Prod Light Server SLIST and thread safety completed Documentation

--- app/elements/server/models/base/light_server/prod_light_server.c
+++ app/elements/server/models/base/light_server/prod_light_server.c
@@ -1,5 +1,15 @@
-#include <prod_light_server.h>
-#include <prod_gen_server.h>
+/**
+ * @file prod_light_server.c
+ * @brief Implementation of the BLE Mesh Lighting Server for the ESP32.
+ *
+ * This file contains the implementation of the BLE Mesh Lighting Server,
+ * including initialization, event handling, and callback registration.
+ *
+ * @author [Pranjal Chanda]
+ */
+
+#include "prod_light_server.h"
+#include "prod_gen_server.h"
 
 #define TAG __func__
 
@@ -7,16 +17,24 @@
 
 static const char* server_state_str[] =
 {
-    [ESP_BLE_MESH_LIGHTING_SERVER_STATE_CHANGE_EVT] = "SERVER_STATE_CHANGE_EVT",
-    [ESP_BLE_MESH_LIGHTING_SERVER_RECV_GET_MSG_EVT] = "SERVER_RECV_GET_MSG_EVT",
-    [ESP_BLE_MESH_LIGHTING_SERVER_RECV_SET_MSG_EVT] = "SERVER_RECV_SET_MSG_EVT",
-    [ESP_BLE_MESH_LIGHTING_SERVER_RECV_STATUS_MSG_EVT] = "SERVER_RECV_STATUS_MSG_EVT",
+    [ESP_BLE_MESH_LIGHTING_SERVER_STATE_CHANGE_EVT] = "STATE_CHANGE_EVT",
+    [ESP_BLE_MESH_LIGHTING_SERVER_RECV_GET_MSG_EVT] = "RECV_GET_MSG_EVT",
+    [ESP_BLE_MESH_LIGHTING_SERVER_RECV_SET_MSG_EVT] = "RECV_SET_MSG_EVT",
+    [ESP_BLE_MESH_LIGHTING_SERVER_RECV_STATUS_MSG_EVT] = "RECV_STATUS_MSG_EVT",
 };
 
 static uint16_t prod_lighting_server_init = 0;
-static uint16_t prod_lighting_server_cb_reg_table_idx = 0;
-static prod_lighting_server_cb_reg_t prod_lighting_server_cb_reg_table[CONFIG_MAX_PROD_LIGHTING_SRV_CB];
+static struct prod_lighting_server_cb_list prod_lighting_server_cb_reg_table = SLIST_HEAD_INITIALIZER(prod_lighting_server_cb_reg_table);
+static SemaphoreHandle_t prod_lighting_server_mutex;
 
+/**
+ * @brief Callback function for BLE Mesh Lightness Server events.
+ *
+ * This function is called whenever a BLE Mesh Lightness Server event occurs.
+ *
+ * @param[in] event The event type for the BLE Mesh Lightness Server.
+ * @param[in] param Parameters associated with the event.
+ */
 static void prod_ble_lightness_server_cb(esp_ble_mesh_lighting_server_cb_event_t event,
                                          esp_ble_mesh_lighting_server_cb_param_t *param)
 {
@@ -24,48 +42,83 @@ static void prod_ble_lightness_server_cb(esp_ble_mesh_lighting_server_cb_event_t
              event, param->ctx.recv_op, param->ctx.addr, param->ctx.recv_dst);
 
     ESP_LOGI(TAG, "%s", server_state_str[event]);
-    for (size_t i = 0; i < prod_lighting_server_cb_reg_table_idx && i < CONFIG_MAX_PROD_LIGHTING_SRV_CB; i++)
-    {
-        if ((prod_lighting_server_cb_reg_table[i].model_id == param->model->model_id
-        || prod_lighting_server_cb_reg_table[i].model_id == param->model->vnd.model_id)
-        && prod_lighting_server_cb_reg_table[i].cb)
-        {
-            /* Despacth callback to lightng model type */
-            prod_lighting_server_cb_reg_table[i].cb(param);
+
+    if (xSemaphoreTake(prod_lighting_server_mutex, portMAX_DELAY) == pdTRUE) {
+        prod_lighting_server_cb_reg_t *entry;
+        SLIST_FOREACH(entry, &prod_lighting_server_cb_reg_table, entries) {
+            if ((entry->model_id == param->model->model_id || entry->model_id == param->model->vnd.model_id) && entry->cb) {
+                /* Dispatch callback to lighting model type */
+                entry->cb(param);
+            }
         }
+        xSemaphoreGive(prod_lighting_server_mutex);
     }
 }
 
+/**
+ * @brief Register a callback function for the lighting server model.
+ *
+ * This function registers a callback function that will be called when
+ * certain events occur in the lighting server model.
+ *
+ * @param[in] model_id  The ID of the lighting server model.
+ * @param[in] cb        The callback function to register.
+ *
+ * @return
+ *    - ESP_OK: Success
+ *    - ESP_ERR_INVALID_ARG: Invalid argument
+ *    - ESP_FAIL: Other failures
+ */
 esp_err_t prod_lighting_reg_cb(uint32_t model_id, prod_lighting_server_cb cb)
 {
-    if(prod_lighting_server_cb_reg_table_idx >= CONFIG_MAX_PROD_LIGHTING_SRV_CB)
-    {
-        ESP_LOGE(TAG, "No Memory left in prod_lighting_server_cb_reg_table");
-        return ESP_ERR_NO_MEM;
-    }
-
-    for (size_t i = 0; i < prod_lighting_server_cb_reg_table_idx; i++)
-    {
-        if (prod_lighting_server_cb_reg_table[i].model_id == model_id)
-        {
-            /* If already registered over-write */
-            prod_lighting_server_cb_reg_table[i].cb = cb;
-            return ESP_OK;
+    if (xSemaphoreTake(prod_lighting_server_mutex, portMAX_DELAY) == pdTRUE) {
+        prod_lighting_server_cb_reg_t *entry;
+        SLIST_FOREACH(entry, &prod_lighting_server_cb_reg_table, entries) {
+            if (entry->model_id == model_id) {
+                /* If already registered, overwrite */
+                entry->cb = cb;
+                xSemaphoreGive(prod_lighting_server_mutex);
+                return ESP_OK;
+            }
         }
-    }
 
-    prod_lighting_server_cb_reg_table[prod_lighting_server_cb_reg_table_idx].cb = cb;
-    prod_lighting_server_cb_reg_table[prod_lighting_server_cb_reg_table_idx++].model_id = model_id;
+        entry = malloc(sizeof(prod_lighting_server_cb_reg_t));
+        if (!entry) {
+            ESP_LOGE(TAG, "No Memory left for prod_lighting_server_cb_reg_table");
+            xSemaphoreGive(prod_lighting_server_mutex);
+            return ESP_ERR_NO_MEM;
+        }
+
+        entry->model_id = model_id;
+        entry->cb = cb;
+        SLIST_INSERT_HEAD(&prod_lighting_server_cb_reg_table, entry, entries);
+        xSemaphoreGive(prod_lighting_server_mutex);
+    }
 
     return ESP_OK;
 }
 
+/**
+ * @brief Initialize the production lighting server.
+ *
+ * This function sets up the necessary configurations and initializes the
+ * production lighting server for the BLE mesh node.
+ *
+ * @return
+ *     - ESP_OK: Success
+ *     - ESP_FAIL: Failed to initialize the lighting server
+ */
 esp_err_t prod_lighting_srv_init(void)
 {
-    if(prod_lighting_server_init == PROD_SERVER_INIT_MAGIC_NO)
+    if (prod_lighting_server_init == PROD_SERVER_INIT_MAGIC_NO)
         return ESP_OK;
+
+    prod_lighting_server_mutex = xSemaphoreCreateMutex();
+    if (prod_lighting_server_mutex == NULL) {
+        ESP_LOGE(TAG, "Failed to create mutex");
+        return ESP_FAIL;
+    }
 
     prod_lighting_server_init = PROD_SERVER_INIT_MAGIC_NO;
     return esp_ble_mesh_register_lighting_server_callback(prod_ble_lightness_server_cb);
 }
-
