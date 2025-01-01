ae9915e7c5778b8853c9cf1829be180e5a1cf5e7
Pranjal <pranjalchanda08@gmail.com>
2025-01-01 16:42:00 +0530

    CI Initial Commit

--- app/scripts/build.sh
+++ app/scripts/build.sh
@@ -2,4 +2,6 @@ TARGET=$1
 
 echo "TARGET = $TARGET"
 python3 scripts/code_gen.py $TARGET
-idf.py -DTARGET="$TARGET" set-target esp32c3 clean build
+APP_PATH=$(pwd)
+sudo docker run --rm -v "$PWD:$APP_PATH" -w "$APP_PATH" -u "$UID" -e HOME=/tmp espressif/idf:release-v5.4 idf.py -DTARGET="$TARGET" set-target esp32c3 build
+# idf.py -DTARGET="$TARGET" set-target esp32c3 clean build
