1bc329ea3cb6017c140fddfe1cf4528205ab7c78
Pranjal <pranjalchanda08@gmail.com>
2025-01-12 03:50:01 +0530

    os-timer control_task driver initial commit

--- app/main/tasks/control_task.h
+++ app/main/tasks/control_task.h
@@ -90,7 +90,12 @@ typedef enum PACKED_ATTR
  */
 typedef enum PACKED_ATTR
 {
-    CONTROL_TASK_MSG_CODE_SYSTEM_MAX, /**< Maximum system event value. */
+    CONTROL_TASK_MSG_EVT_SYSTEM_RESTART      = BIT0,    /**< Event to restart the system. */
+    CONTROL_TASK_MSG_EVT_SYSTEM_TIMER_ARM    = BIT1,    /**< Event to arm an OS Timer */
+    CONTROL_TASK_MSG_EVT_SYSTEM_TIMER_DISARM = BIT2,    /**< Event to stop an OS Timer */
+    CONTROL_TASK_MSG_EVT_SYSTEM_TIMER_DELETE = BIT3,    /**< Event to del an OS Timer */
+    CONTROL_TASK_MSG_EVT_SYSTEM_TIMER_FIRE   = BIT4,    /**< Event to fire timedout OS Timer */
+    CONTROL_TASK_MSG_EVT_SYSTEM_MAX, /**< Maximum system event value. */
 } control_task_msg_evt_system_t;
 
 /**
