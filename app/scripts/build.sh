bf4493e912eef5eb3e2cf40d338effcd99acb8c4
Pranjal <pranjalchanda08@gmail.com>
2025-09-06 19:49:43 +0530

    Add clean in build.sh

--- app/scripts/build.sh
+++ app/scripts/build.sh
@@ -3,5 +3,13 @@ TARGET=$1
 echo "TARGET = $TARGET"
 python3 scripts/code_gen.py $TARGET
 APP_PATH=$(pwd)
-idf.py -DTARGET="$TARGET" set-target esp32c3 clean build
+BUILD_PARAM=""
+echo $2
+# clean only if $2 is "clean"
+if [ "$2" = "clean" ]; then
+    echo "Cleaning build..."
+    rm -rfv build/*
+    BUILD_PARAM="set-target esp32c3 clean"
+fi
+idf.py -DTARGET="$TARGET" $BUILD_PARAM build
 # sudo docker run --rm -v "$PWD:$APP_PATH" -w "$APP_PATH" -u "$UID" -e HOME=/tmp espressif/idf:release-v5.4 idf.py -DTARGET="$TARGET" set-target esp32c3 build
