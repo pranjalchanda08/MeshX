a8ec75ff9596a1926c203c5ac3d62d55dfd87dfe
Pranjal Chanda <40349163+pranjalchanda08@users.noreply.github.com>
2025-09-27 10:35:46 +0000

    Refactor CMake configuration and add product profile for WeAct C3 board

--- port/platform/esp/esp32c3/esp32c3.cmake
+++ port/platform/esp/esp32c3/esp32c3.cmake
@@ -1,37 +1,28 @@
 cmake_minimum_required(VERSION 3.12)
 
+set(ESP_TARGET esp32c3 CACHE STRING "ESP target (e.g., esp32, esp32c3)")
+
+message(STATUS CMAKE_SORCE_DIR: ${CMAKE_SOURCE_DIR})
+
+set(PLATFORM_TEST_SUPPORTED ON)
+set(IDF_TARGET ${ESP_TARGET})
+set(SDKCONFIG "${CMAKE_SOURCE_DIR}/port/platform/esp/esp32c3/config/sdkconfig.defaults" CACHE STRING "sdkconfig path")
+
 include("${CMAKE_SOURCE_DIR}/port/platform/esp/esp_idf/esp_idf.cmake")
 
 set(PLATFORM_INC
     ${BASE_PLAT_INC}
     "${CMAKE_SOURCE_DIR}/port/platform/esp/esp32c3/config"
-    PARENT_SCOPE
+    CACHE STRING "platform include"
 )
 
-# add_library(plat_esp32c3 STATIC)
+set(INC_FILES
+    ${INC_FILES}
+    ${PLATFORM_INC}
+    ${CMAKE_SOURCE_DIR}/main/component/meshx/inc
+)
 
-# target_sources(plat_esp32c3 PUBLIC
-#     ${ALL_SRC_FILES}
-# )
-
-# target_include_directories(plat_esp32c3 PUBLIC
-#     ${FINAL_INC_DIRS}
-# )
-# -------------------------------
-# Register the ESP-IDF component
-# -------------------------------
-# idf_component_register(
-#     SRCS         ${ALL_SRC_FILES}
-#     INCLUDE_DIRS ${FINAL_INC_DIRS}
-#     REQUIRES
-#         bt
-#         console
-#         nvs_flash
-#         esp_driver_uart
-# )
-
-# target_link_libraries(plat_esp32c3 PUBLIC meshx)
-# target_link_libraries(plat_esp32c3 PUBLIC osal_FreeRTOS)
-
-# Optional: if you have compile definitions or extra flags
-# target_compile_definitions(${TARGET} PUBLIC SOME_DEFINE=1)
+set(SRC_FILES
+    ${SRC_FILES}
+    ${BASE_PLAT_SRC}
+)
--- port/platform/esp/esp_idf/esp_idf.cmake
+++ port/platform/esp/esp_idf/esp_idf.cmake
@@ -1,28 +1,59 @@
 set(ESP_COMMON_DIR "${CMAKE_SOURCE_DIR}/port/platform/esp/esp_idf")
 
+message(STATUS "ESP_TARGET: ${ESP_TARGET}")
+message(STATUS "IDF_PATH: $ENV{IDF_PATH}")
+message(STATUS "SDKCONFIG: ${SDKCONFIG}")
+
 include($ENV{IDF_PATH}/tools/cmake/project.cmake)
+
+# Library registration function for ESP-IDF
+function(register_component SRC INC LIBS)
+    idf_component_register(
+        SRCS ${SRC}
+        INCLUDE_DIRS ${INC}
+        REQUIRES ${LIBS}
+    )
+endfunction()
 
 # -------------------------------
 # Recursively collect all source files (.c)
 # -------------------------------
-file(GLOB_RECURSE ALL_SRC_FILES
-CONFIGURE_DEPENDS
+file(GLOB_RECURSE ESP_SRC
+    CONFIGURE_DEPENDS
     "${ESP_COMMON_DIR}/**/*.c"
     )
 
 # Recursively collect all directories within the base path.
-file(GLOB_RECURSE ALL_DIRS
+file(GLOB_RECURSE ESP_INC_DIRS
     LIST_DIRECTORIES true
     "${ESP_COMMON_DIR}/**"
     )
 
 # Use a regular expression to filter for directories ending with /inc or /include.
-list(FILTER ALL_DIRS INCLUDE REGEX "[/\\](inc|include)$")
+list(FILTER ESP_INC_DIRS INCLUDE REGEX "[/\\](inc|include)$")
 
-message(STATUS "ALL DIRS found: ${ALL_DIRS}")
+message(STATUS "ESP_SRC: ${ESP_SRC}")
+message(STATUS "ESP_INC: ${ESP_INC_DIRS}")
 
 # Remove duplicates from the filtered list.
-list(REMOVE_DUPLICATES ALL_DIRS)
+list(REMOVE_DUPLICATES ESP_INC_DIRS)
 
 # Now set FINAL_INC_DIRS from the filtered list.
-set(BASE_PLAT_INC "${ALL_DIRS}")
+set(BASE_PLAT_INC "${ESP_INC_DIRS}")
+set(BASE_PLAT_SRC "${ESP_SRC}")
+
+set(PLAT_LIBS
+    ${PLAT_LIBS}
+    bt
+    nvs_flash
+    console
+    esp_driver_uart
+    freertos
+)
+
+add_definitions(-DCONFIG_APP_MAIN=app_main)
+
+set(INC_FILES
+    ${INC_FILES}
+    ${BASE_PLAT_INC}
+)
