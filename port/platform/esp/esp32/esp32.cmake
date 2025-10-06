cbc32cb4eaeb09434a6d4a8d97870e18c80e0fa4
Pranjal Chanda <40349163+pranjalchanda08@users.noreply.github.com>
2025-10-06 18:08:24 +0000

    BSP: ESP32_devkitC build addition

--- port/platform/esp/esp32/esp32.cmake
+++ port/platform/esp/esp32/esp32.cmake
@@ -1,19 +1,19 @@
 cmake_minimum_required(VERSION 3.12)
 
-set(ESP_TARGET esp32c3 CACHE STRING "ESP target (e.g., esp32, esp32c3)")
+set(ESP_TARGET esp32 CACHE STRING "ESP target")
 
 message(STATUS CMAKE_SORCE_DIR: ${CMAKE_SOURCE_DIR})
 
 set(PLATFORM_TEST_SUPPORTED ON)
 set(IDF_TARGET ${ESP_TARGET})
-file(GLOB SDKCONFIG_DEFAULTS_LIST "${CMAKE_SOURCE_DIR}/port/platform/esp/esp32c3/sdkconfig.defaults*")
+file(GLOB SDKCONFIG_DEFAULTS_LIST "${CMAKE_SOURCE_DIR}/port/platform/esp/esp32/sdkconfig.defaults*")
 set(SDKCONFIG_DEFAULTS "${SDKCONFIG_DEFAULTS_LIST}" CACHE STRING "sdkconfig path")
 
 include("${CMAKE_SOURCE_DIR}/port/platform/esp/esp_idf/esp_idf.cmake")
 
 set(PLATFORM_INC
     ${BASE_PLAT_INC}
-    "${CMAKE_SOURCE_DIR}/port/platform/esp/esp32c3"
+    "${CMAKE_SOURCE_DIR}/port/platform/esp/esp32"
     CACHE STRING "platform include"
 )
 
