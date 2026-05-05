f001bec5b4e5b4c9a24b0d9cd631cd4012f4b07f
Pranjal Chanda <pranjalchanda08@gmail.com>
2026-05-06 01:22:50 +0530

    Introduce platform-specific provisioning event codes and expand event handling for mesh provisioning server.
    Bootup bug fixes

--- main/component/meshx/ble_mesh/elements/src/variants/meshx_relay_element.cpp
+++ main/component/meshx/ble_mesh/elements/src/variants/meshx_relay_element.cpp
@@ -245,6 +245,11 @@ meshx_err_t meshXRelayServerElement::s_prov_cb(
     {
     case CONTROL_TASK_MSG_EVT_EN_NODE_PROV:
     {
+        if (!meshx_prov_srv_is_provisioned())
+        {
+            return MESHX_SUCCESS;
+        }
+
         auto elements = meshXElementRegistry::get_instance().get_all_elements();
         for (auto const& [abs_id, base_el] : elements)
         {
